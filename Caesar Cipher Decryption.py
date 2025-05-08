98bc76404b117454699ca611d9ccaf5903de8af1
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 04:54:02 +0530

    Update Caesar Cipher Decryption.py

--- Caesar Cipher Decryption.py
+++ Caesar Cipher Decryption.py
@@ -1,1 +1,26 @@
+# fuction with two parameters 
+def caesar_decrypt(text, shift):
 
+    # Define the variables 
+    result = ''
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    for char in text.lower():
+        if char in alphabet:
+
+            # Subtract shifting value to the text
+            index = (alphabet.index(char) - shift) % len(alphabet)
+            result += alphabet[index]
+        else:
+            
+            # leave spaces and punctuation unchanged
+            result += char  
+    return result
+
+# Get the inputs from the user
+text = str(input("Enter the text: "))
+shift = int(input("Enter your shifting value: "))
+
+# Calling the function 
+decrypted = caesar_decrypt(text, shift)
+print("Decrypted text: ", decrypted)
