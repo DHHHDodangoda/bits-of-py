6808e29156374bd21f43fc04f1b91f26c9997764
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 10:19:05 +0530

    Update Two dimensional list.py

--- Learned Concepts/Lists/Two dimensional list.py
+++ Learned Concepts/Lists/Two dimensional list.py
@@ -1,1 +1,9 @@
+# Create a two dimensional list
+# This will show as a table
 
+List01 = [[3,5,1],[7,3,9],[6,0,4]]
+
+# To display values
+
+print(List01[1][2]) # 2nd index in the 1st item / 9
+print(List01[0][0]) # first item on the first item
