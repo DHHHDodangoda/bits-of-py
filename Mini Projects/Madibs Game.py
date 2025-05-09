8e8750cf88e0fbf8fb87e34702b24fecc6de3e6a
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 07:22:19 +0530

    Update Madibs Game.py

--- Mini Projects/Madibs Game.py
+++ Mini Projects/Madibs Game.py
@@ -2,6 +2,7 @@
 
 print("Welcome to the Madlibs game!")
 
+# Get the user inputs
 while True:
     adjective01 = input("Please enter an adjective: ")
     noun01 = input("Please enter a noun: ")
@@ -9,9 +10,11 @@ while True:
     verb01 = input("Please enter a verb: ")
     adjective03 = input("Please enter one more adjective: ")
 
+    # Create the story
     print ("Here is your story...\n")
     print (f"Today I went to a {adjective01} zoo. In the exhibit, I saw a {noun01}. {noun01} was {adjective02} and {verb01}. I was {adjective03}.")
-    
+
+    # Exit or play again
     while True:
         again = input("\nDo you want to play again (yes/no): \n").strip().lower()
         if again == 'yes':
