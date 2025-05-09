02b68e8a2cff2ec4d6c25a8989dc4b80311cb303
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 10:10:06 +0530

    Update Removing methods.py

--- Learned Concepts/Lists/Removing methods.py
+++ Learned Concepts/Lists/Removing methods.py
@@ -2,3 +2,18 @@
 
 PrimaryList = [20,'Amal','Amara',39.534,80,"Mango"]
 SecondaryList = ['item 01' , 'item 02' , 'item 03']
+
+# Delete items wtih del statement 
+
+del(PrimaryList(4:5)
+print (PrimaryList)
+
+# Remove items using remove method
+
+PrimaryList.remove('Amal')
+print (PrimaryList)
+
+# Remove index item in a list
+
+PrimaryList.pop(2)
+print (PrimaryList)
