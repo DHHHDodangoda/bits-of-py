02b75ffda413458a1269c6649316ff371276943a
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 10:58:24 +0530

    Update If elif else statement.py

--- Learned Concepts/Control Structures & loops/If elif else statement.py
+++ Learned Concepts/Control Structures & loops/If elif else statement.py
@@ -1,1 +1,25 @@
+'''
+if (condition 01):
+  result 1
+elif (condition 02):
+  result 2
+elif (condition 3):
+  result 3
+else:
+  result 4
+'''
 
+# Get the user input
+
+x = int(input("Enter your number: ")
+
+# Show a message
+        
+if x > 10:
+  print('Number is lower than 10')
+elif x > 100:
+  print ('Number is lower than 100')
+elif x > 500:
+  print ('Number is lower than 500')
+else:
+  print ('Number is higer than 500')
