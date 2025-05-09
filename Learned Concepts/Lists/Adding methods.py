90c024d020703ad31c8b270c476a4c2774e0963d
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 09:59:03 +0530

    Update Adding methods.py

--- Learned Concepts/Lists/Adding methods.py
+++ Learned Concepts/Lists/Adding methods.py
@@ -1,1 +1,19 @@
+# Creating new lists
 
+PrimaryList = [20,'Amal','Amara',39.534,80,"Mango"]
+SecondaryList = ['item 01' , 'item 02' , 'item 03']
+
+# Adding items to end of the list
+
+PrimeryList.append("Orange")
+print (PrimeryList)
+
+# Adding items to a specific index
+
+PrimeryList.insert(2,65) # in this method items dose not replace 
+print (PrimeryList)
+
+# replace items in a list
+
+PrimeryList[0] = (42)
+print (PrimeryList)
