f6f44387da135813a3a6905a2096af9ae52fcc11
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 10:45:16 +0530

    Update If statement.py

--- Learned Concepts/Control Structures & loops/If statement.py
+++ Learned Concepts/Control Structures & loops/If statement.py
@@ -1,1 +1,14 @@
+x = int(input('Enter your first number: ')
+y = int(input('Enter your second number: ')
 
+# If ( condition 01 ):
+if x == y:
+
+  # print ( result 1 )
+  print('Numbers are equal')
+
+# else:
+else:
+
+  # print ( result 2 )
+  print('Numbers are not equal')
