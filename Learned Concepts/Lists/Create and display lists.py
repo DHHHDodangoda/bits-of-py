12448d404e37aad9726c5a82a0b4df32dfd83683
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 09:50:50 +0530

    Update Create and display lists.py

--- Learned Concepts/Lists/Create and display lists.py
+++ Learned Concepts/Lists/Create and display lists.py
@@ -2,3 +2,14 @@
 
 PrimaryList = [20,'Amal','Amara',39.534,80,"Mango"]
 SecondaryList = ['item 01' , 'item 02' , 'item 03']
+
+# Display the list items
+
+print (PrimeryList) # Full list
+print (PrimeryLiat[3]) # 3rd index / 4th item
+print (PrimeryList[0]) # 0th index / 1st item
+
+print (PrimaryList[1:4]) # 1st index to 4th index
+print (PrimaryList[2:]) # 2nd index and all items after it
+print (PrimaryList[:5]) # All items before 5th index
+print (PrimaryList[:]) # All items in the list
