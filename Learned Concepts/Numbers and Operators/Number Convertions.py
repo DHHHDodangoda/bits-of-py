b604587d17f3890a95126462b5e57a3129e16ac4
HiViN <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-09 08:48:07 +0530

    Update

---
+++ Learned Concepts/Numbers and Operators/Number Convertions.py
@@ -0,0 +1,12 @@
+y = 56.475
+print (y) # 56.475
+y = int(y)
+print (y) # 56
+y = float(y)
+print (y) # 56.0
+y = complex(y)
+print (y) # (56.0+0j)
+y = str(y)
+print (y) # (56.0+0j)
+y = bool(y)
+print (y) # True
