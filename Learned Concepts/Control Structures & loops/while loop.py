2ad0de0b944e4f9758792e583be80f2e70fdac1f
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-13 09:59:42 +0530

    Update while loop.py

--- Learned Concepts/Control Structures & loops/while loop.py
+++ Learned Concepts/Control Structures & loops/while loop.py
@@ -1,1 +1,27 @@
 
+i = 0
+while i in range(5):
+    print(i)
+    i += 1
+# Output: 0, 1, 2, 3, 4
+
+i = 2
+while i in range(2, 10, 2):
+    print(i)
+    i += 2
+# Output: 2, 4, 6, 8
+
+#range() itself doesn’t control the loop — you must manually manage the counter (i).
+#The loop runs as long as i is a valid value in the range object.
+
+while True:
+  print("Hello world")
+  break
+
+count = 0
+while count <5:
+  print(count)
+  count=+1
+print ("done")
+# output: 0, 1, 2, 3, 4, done
+
