1599dbe1c424e794f8885ee3bf7cfa57db6c4602
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-13 08:47:20 +0530

    Update for loops.py

--- Learned Concepts/Control Structures & loops/for loops.py
+++ Learned Concepts/Control Structures & loops/for loops.py
@@ -1,1 +1,13 @@
+'''
+for i in range (1,6):
+  print(i)
+              this will print 1 to 5 *starting value will always print and end value does not print number range will stop printing at (end value -1)
+'''
+
+# Print numbers from 0 to 9
+i=0
+for i in range(0,10):
+    print("i=",i)
+print("done!")
+#print done! when the loop ends
 
