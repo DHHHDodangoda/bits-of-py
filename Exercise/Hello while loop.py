e79b5f942b6fd50e61856eedfa06572c0ed91ae6
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-17 14:18:30 +0530

    Update Hello while loop.py

--- Exercise/Hello while loop.py
+++ Exercise/Hello while loop.py
@@ -1,1 +1,12 @@
+#initialize the variables
+count=0
 
+#make a list of names
+Namelist= ["sam","lisa","micha","dave","wyatt","emma","sage"]
+
+#loop until say hello to every name
+while count<7:
+    
+    print("hello",Namelist[count])
+    count=count+1
+  
