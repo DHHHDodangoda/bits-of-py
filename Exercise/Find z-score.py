b243ef6b1f94f0abb8db248be02bf68d2869bf44
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-17 13:54:11 +0530

    Update Find z-score.py

--- Exercise/Find z-score.py
+++ Exercise/Find z-score.py
@@ -1,1 +1,13 @@
+#initalize the variables
+Zscore=0
+
+#Get the Zcore from the user
+Zscore=float(input("Enter your Z-core"))
+
+#calculate and show the result
+if Zscore>2:
+    print("You can be selected for the engeering program")
+else:
+    print("You can not be selected for the engeering program")
+
 
