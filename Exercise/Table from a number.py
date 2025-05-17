2fd0239a9134001ee26d42bc1fed11738eb1456c
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-17 20:04:12 +0530

    Update Table from a number.py

--- Exercise/Table from a number.py
+++ Exercise/Table from a number.py
@@ -4,3 +4,16 @@ for i in range (1,6):
     print(number,"x",i,"=", number * i)
 
 
+number=0
+
+number = int( input(' Enter a number:'))
+
+for i in range (6):
+    print(number,'x',i,'=',number*i)
+# Enter a number:5
+# 5 x 0 = 0
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# 5 x 4 = 20
+# 5 x 5 = 25
