6b0a8a51fcb1b0bc4a24e48ef46ff0906b3c44c2
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-18 16:59:22 +0530

    Update The zip function.py

--- Learned Concepts/The zip function.py
+++ Learned Concepts/The zip function.py
@@ -1,1 +1,7 @@
+#The zip() function combines multiple iterables (like lists or tuples) into pairs, element by element.
 
+names = ['Alice', 'Bob', 'Charlie']
+scores = [85, 90, 95]
+
+for name, score in zip(names, scores):
+    print(f"{name} scored {score}")
