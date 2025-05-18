b47e6b9fa1f54744ee40e8d3efdbbb5c8a55d12c
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-18 17:03:54 +0530

    Update List Comprehension.py

--- Learned Concepts/Lists/List Comprehension.py
+++ Learned Concepts/Lists/List Comprehension.py
@@ -1,1 +1,13 @@
+#List comprehension is a concise way to create lists using a single line of code.
 
+squares = [x**2 for x in range(5)]
+print(squares)
+
+#Output [0, 1, 4, 9, 16]
+
+''' With a condition '''
+
+evens = [x for x in range(10) if x % 2 == 0]
+print(evens)
+
+#Output [0, 2, 4, 6, 8]
