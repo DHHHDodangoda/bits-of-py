fff6d1c78633e3434aecdba00239ac596501a5e8
Hivin Hiruna <169910955+DHHHDodangoda@users.noreply.github.com>
2025-05-24 03:01:38 +0530

    Update Calculator with python.py

--- Mini Projects/Calculator with python.py
+++ Mini Projects/Calculator with python.py
@@ -3,3 +3,7 @@ import math  # Importing math module for advanced mathematical operations
 # Define a function to add two numbers
 def add(a, b):
     return a + b
+
+# Define a function to subtract second number from first
+def subtract(a, b):
+    return a - b
