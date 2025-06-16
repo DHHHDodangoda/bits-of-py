3c5a1c8d7b00eff4dd0d820b72560eafd637bb4d
HiViN <169910955+DHHHDodangoda@users.noreply.github.com>
2025-06-16 21:14:50 +0530

    add Module_File_01.py and Module_File_02.py with function definitions

---
+++ Learned Concepts/Packages and Modules/Main_File.py
@@ -0,0 +1,12 @@
+from Package_File import Module_File_01
+
+Module_File_01.FirstFunction()
+
+Module_File_01.SecondFunction()
+
+Module_File_01.ThirdFunction()
+
+# Output:
+# This is the first function.
+# This is the second function.
+# This is the third function.
---
+++ Learned Concepts/Packages and Modules/Package_File/Module_File_02.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print("Hello, World from Module File 02!")
+
+def another_function():
+    print("This is another function in Module File 02.")
