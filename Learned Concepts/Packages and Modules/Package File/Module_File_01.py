7d74c552709c9ba66cc70dee983bb87c5a492841
HiViN <169910955+DHHHDodangoda@users.noreply.github.com>
2025-06-16 21:06:11 +0530

    add Module_File_01.py with initial function definitions

---
+++ Learned Concepts/Packages and Modules/Package File/Module_File_01.py
@@ -0,0 +1,8 @@
+def FirstFunction():
+    print("This is the first function.")
+
+def SecondFunction():
+    print("This is the second function.")
+
+def ThirdFunction():
+    print("This is the third function.")
