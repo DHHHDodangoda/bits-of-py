31d4462a1f46e3e09194404cd3cdc8e4bb3239c4
HiViN <169910955+DHHHDodangoda@users.noreply.github.com>
2025-06-18 04:13:31 +0530

    update Main_File.py to include additional function calls from Module_File_02

--- Learned Concepts/Packages and Modules/Main_File.py
+++ Learned Concepts/Packages and Modules/Main_File.py
@@ -10,3 +10,17 @@ Module_File_01.ThirdFunction()
 # This is the first function.
 # This is the second function.
 # This is the third function.
+
+
+from Package_File.Module_File_02 import hello_world
+
+hello_world()
+
+from Package_File.Module_File_02 import another_function as secOne
+
+secOne()
+
+# Output:
+# Hello, World from Module File 02!
+# This is another function in Module File 02.
+
